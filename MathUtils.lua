519d2540beaf34dea469d7b9c6ba26594d3b8fa4
TringForAllTheBadges <ericmgreen2006@gmail.com>
2024-08-29 12:33:22 -0500

    Update MathUtils.lua

--- MathUtils.lua
+++ MathUtils.lua
@@ -13,4 +13,16 @@ function MathUtils.round(n, d)
     return math.floor(n * m + 0.5) / m
 end
 
+function MathUtils.map(v, a1, a2, b1, b2)
+    return b1 + (v - a1) * (b2 - b1) / (a2 - a1)
+end
+
+function MathUtils.dist(x1, y1, x2, y2)
+    return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
+end
+
+function MathUtils.angleto(x1, y1, x2, y2)
+    return math.atan2(y2 - y1, x2 - x1)
+end
+
 return MathUtils
