db68be06d8263706eec0d92d2d502fd6453c71a4
TringForAllTheBadges <ericmgreen2006@gmail.com>
2024-08-29 12:29:19 -0500

    Create ColorUtils.lua

---
+++ ColorUtils.lua
@@ -0,0 +1,29 @@
+local ColorUtils = {}
+
+function ColorUtils.rgbtohsv(r, g, b)
+    r, g, b = r/255, g/255, b/255
+    local max, min = math.max(r, g, b), math.min(r, g, b)
+    local h, s, v
+    v = max
+
+    local d = max - min
+    if max == 0 then s = 0 else s = d / max end
+
+    if max == min then
+        h = 0
+    else
+        if max == r then
+            h = (g - b) / d
+            if g < b then h = h + 6 end
+        elseif max == g then
+            h = (b - r) / d + 2
+        else
+            h = (r - g) / d + 4
+        end
+        h = h / 6
+    end
+
+    return h, s, v
+end
+
+return ColorUtils
